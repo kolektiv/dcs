714d83f9e2ff649ed3456c647633f2abf157a0fc
kolektiv <andrew@xyncro.com>
2021-02-15 16:55:23 +0000

    rtd

--- dcs-contrib-hue/src/lib.rs
+++ dcs-contrib-hue/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use async_trait::async_trait;
+use dcs::sensors::PullSensor;
+
+pub struct HueLight;
+
+#[async_trait]
+impl PullSensor<i32> for HueLight {
+    async fn read(&mut self) -> anyhow::Result<i32> {
+        todo!()
     }
 }
--- dcs-contrib-sequent-rtd/src/lib.rs
+++ dcs-contrib-sequent-rtd/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use async_trait::async_trait;
+use anyhow::Result;
+use dcs::sensors::PullSensor;
+
+#[derive(Debug)]
+pub struct RTD;
+
+#[async_trait]
+impl PullSensor<i32> for RTD {
+    async fn read(&mut self) -> Result<i32> {
+        todo!()
     }
 }
